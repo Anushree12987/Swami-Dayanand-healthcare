6bec6cb0672f41b0b9c0bc80d2f6ccf637063a1d
Anushree12987 <anushreegami@icloud.com>
2026-04-02 04:24:09 +0545

    Updated PatientDashboard.jsx page

--- gamicare/hospital-system/public-website/src/pages/PatientDashboard.jsx
+++ gamicare/hospital-system/public-website/src/pages/PatientDashboard.jsx
@@ -96,7 +96,7 @@ const PatientDashboard = () => {
 
   const formatDate = (dateString) => {
     const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
-    return new Date(dateString).toLocaleDateString('en-US', options);
+    return new Date(dateString).toLocaleDateString('en-IN', options);
   };
 
   if (loading) {
